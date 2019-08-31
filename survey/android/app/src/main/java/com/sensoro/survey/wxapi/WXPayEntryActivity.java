27496f45667175cccca38ca178caf6ac1c198036
chunwanli <liwanchun.love@163.com>
2019-08-31 14:25:32 +0800

    暂时屏蔽android分享

--- survey/android/app/src/main/java/com/sensoro/survey/wxapi/WXPayEntryActivity.java
+++ survey/android/app/src/main/java/com/sensoro/survey/wxapi/WXPayEntryActivity.java
@@ -1,8 +1,8 @@
-package com.sensoro.survey.wxapi;
-
-import com.jarvan.fluwx.wxapi.FluwxWXEntryActivity;
-
-public class WXPayEntryActivity extends FluwxWXEntryActivity {
-
-
-}
+//package com.sensoro.survey.wxapi;
+//
+//import com.jarvan.fluwx.wxapi.FluwxWXEntryActivity;
+//
+//public class WXPayEntryActivity extends FluwxWXEntryActivity {
+//
+//
+//}
