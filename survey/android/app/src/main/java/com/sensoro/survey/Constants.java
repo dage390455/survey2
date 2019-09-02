5e0e6e2e7933b1e70e433e4b3b22805f602ac2df
wlp <wlpgdd0725>
2019-09-02 14:15:31 +0800

    Android原生微信分享

---
+++ survey/android/app/src/main/java/com/sensoro/survey/Constants.java
@@ -0,0 +1,5 @@
+package com.sensoro.survey;
+
+public class Constants {
+    public static final String APP_ID = "wxa65d8bad62a982e1";
+}
--- survey/android/app/src/main/java/com/sensoro/survey/MainActivity.java
+++ survey/android/app/src/main/java/com/sensoro/survey/MainActivity.java
@@ -14,15 +14,30 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.tencent.mm.opensdk.modelmsg.GetMessageFromWX;
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import java.util.Collection;
+import java.util.Map;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.BasicMessageChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.StringCodec;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.FlutterNativeView;
 import io.flutter.view.FlutterView;
 
+import static com.tencent.mm.opensdk.modelmsg.SendMessageToWX.Req.WXSceneSession;
+
 public class MainActivity extends FlutterActivity {
 
+    public static IWXAPI api;
 
     private String routeStr = "projectList";  //首页路径
     BasicMessageChannel channel1;
@@ -38,6 +53,40 @@ public class MainActivity extends FlutterActivity {
         flutterView.setLayoutParams(matchParent);
         flutterView.enableTransparentBackground();
         this.setContentView(flutterView);
+
+
+        new MethodChannel(flutterView, "com.pages.your/project_list").setMethodCallHandler(
+                (call, result) -> {
+                    //调用分享
+                    if (call.method.equals("outputDocument")) {
+                        boolean wxAppInstalled = api.isWXAppInstalled();
+                        if (wxAppInstalled) {
+                            Map<String, Map> map = (Map) call.arguments;
+                            Map value = null;
+                            for (String key : map.keySet()) {
+                                value = map.get(key);
+                                System.out.println(key + ":" + value);
+                            }
+                            if (null != value) {
+                                WXTextObject textObj = new WXTextObject();
+                                textObj.text = value.toString();
+                                WXMediaMessage msg = new WXMediaMessage();
+                                msg.mediaObject = textObj;
+                                msg.description = value.toString();
+                                SendMessageToWX.Req req = new SendMessageToWX.Req();
+                                req.transaction = String.valueOf(System.currentTimeMillis());
+                                req.message = msg;
+                                req.scene = WXSceneSession;
+                                api.sendReq(req);
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "当前手机未安装微信，请安装后重试", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
         final BasicMessageChannel channel = new BasicMessageChannel<String>(flutterView, "BasicMessageChannelPlugin", StringCodec.INSTANCE);
         channel.setMessageHandler((o, reply) -> {
 
@@ -68,7 +117,6 @@ public class MainActivity extends FlutterActivity {
                 double longitude = data.getDoubleExtra("longitude", -1);
                 String title = data.getStringExtra("title");
                 channel1.send(title);
-//                Log.d("===title", "title------- = " + title + ",latitude = =" + latitude + "==longitude==" + longitude);
             }
         }
 
@@ -92,15 +140,14 @@ public class MainActivity extends FlutterActivity {
         }
     }
 
-    public void sendMessageToFlutter() {
-        channel1.send("哈哈哈");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
 
+        api = WXAPIFactory.createWXAPI(this, Constants.APP_ID, false);
+        api.registerApp(Constants.APP_ID);
     }
 
 
